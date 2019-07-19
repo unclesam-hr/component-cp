415dfe3dcb48983377f243d3b672bff9b2f67e14
Christopher Pham <chrismpham@gmail.com>
2019-07-19 14:13:42 -0700

    reseeded AWS RDS to match refactored carousel.

--- database/index.js
+++ database/index.js
@@ -1,17 +1,18 @@
 var Credentials = require('../credentials.js');
 const Sequelize = require('sequelize');
 //                                  db           user       pw             options
-// AWS RDS
-// const connection = new Sequelize('fecwestelms', 'admin', Credentials.mypw, {
-//     host : Credentials.host,
-//     port: '3306',
-//     dialect : 'mysql'
-// });
-
-const connection = new Sequelize('fecwestelms', 'root', '', {
-    host : 'localhost',
+// AWS RDS WITH MYSQL ENGINE
+const connection = new Sequelize('fecwestelms', 'admin', Credentials.mypw, {
+    host : Credentials.host,
+    port: '3306',
     dialect : 'mysql'
 });
+
+// LOCALIZED DB WITH MYSQL ENGINE
+// const connection = new Sequelize('fecwestelms', 'root', '', {
+//     host : 'localhost',
+//     dialect : 'mysql'
+// });
 
 connection
     .authenticate()
